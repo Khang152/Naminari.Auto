52660c0529f3592f795006edc895b2f4b7163246
khang152 <khang152.tk@gmail.com>
2023-03-08 03:13:35 +0700

    Update CommandAsync method to send command text input

--- Naminari.Auto/Naminari.Auto/Libraries/Keyboard.cs
+++ Naminari.Auto/Naminari.Auto/Libraries/Keyboard.cs
@@ -6,6 +6,8 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Automation;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 
 namespace Naminari.Auto.Libraries
@@ -53,9 +55,8 @@ namespace Naminari.Auto.Libraries
         public static async Task<bool> CommandAsync(string commandKeys, string? processName = null)
         {
             if (string.IsNullOrWhiteSpace(processName))
-            {
-                SendKeys.SendWait(commandKeys);
-                return await Task.FromResult(true);
+            {             
+                return await SendKeysCombination(commandKeys);
             }
 
             Process? process = Process.GetProcessesByName(processName).FirstOrDefault();
@@ -65,8 +66,7 @@ namespace Naminari.Auto.Libraries
                 {
                     IntPtr handle = process.MainWindowHandle;
                     SetForegroundWindow(handle);
-                    SendKeys.SendWait(commandKeys);
-                    return await Task.FromResult(true);
+                    return await SendKeysCombination(commandKeys);
                 }
                 finally
                 {
@@ -127,6 +127,143 @@ namespace Naminari.Auto.Libraries
         public static KeyItems KeysCombined(FunctionKeys functionKeys) => new KeyItems { FunctionKey = functionKeys };
 
         public static KeyItems KeysRepeating(Keys key) => new KeyItems { RepeatingKey = key };
+
+        public static Task<bool> SendKeysCombination(string keysCombination)
+        {
+            // Split the keys combination string into individual keys
+            string[] keysRelease = keysCombination.Split(',');
+            string[] keysPress = new string[0];
+
+            string keyRelease = string.Empty;
+            if (keysRelease.Length > 1)
+            {
+                keysPress = keysRelease[0].Split('+');
+                keyRelease = keysRelease[1];
+            }
+            else
+            {
+                keysPress = keysCombination.Split('+');
+            }
+
+            // Convert each key to the corresponding SendKeys format
+            string formattedKeys = "";
+            foreach (string key in keysPress)
+            {
+                formattedKeys += ConvertKeyToFormat(key);
+            }
+
+            // Simulate the key press using SendKeys.SendWait
+            SendKeys.SendWait(formattedKeys.ToLower());
+            if (!string.IsNullOrWhiteSpace(keyRelease))
+            {
+                SendKeys.SendWait(ConvertKeyToFormat(keyRelease));
+            }
+
+            return Task.FromResult(true);
+        }
+
+        public static string ConvertKeyToFormat(string key)
+        {
+            // Convert the key string to the corresponding SendKeys format
+            switch (key.ToLower())
+            {
+                case "ctrl":
+                    return "^";
+                case "alt":
+                    return "%";
+                case "shift":
+                    return "+";
+                case "enter":
+                    return "{ENTER}";
+                case "tab":
+                    return "{TAB}";
+                case "esc":
+                case "escape":
+                    return "{ESC}";
+                case "home":
+                    return "{HOME}";
+                case "end":
+                    return "{END}";
+                case "left":
+                    return "{LEFT}";
+                case "right":
+                    return "{RIGHT}";
+                case "up":
+                    return "{UP}";
+                case "down":
+                    return "{DOWN}";
+                case "pgup":
+                    return "{PGUP}";
+                case "pgdn":
+                    return "{PGDN}";
+                case "numlock":
+                    return "{NUMLOCK}";
+                case "scrolllock":
+                    return "{SCROLLLOCK}";
+                case "prtsc":
+                    return "{PRTSC}";
+                case "break":
+                    return "{BREAK}";
+                case "backspace":
+                case "bksp":
+                case "bs":
+                    return "{BACKSPACE}";
+                case "clear":
+                    return "{CLEAR}";
+                case "capslock":
+                    return "{CAPSLOCK}";
+                case "ins":
+                case "insert":
+                    return "{INSERT}";
+                case "del":
+                case "delete":
+                    return "{DELETE}";
+                case "help":
+                    return "{HELP}";
+                case "f1":
+                    return "{F1}";
+                case "f2":
+                    return "{F2}";
+                case "f3":
+                    return "{F3}";
+                case "f4":
+                    return "{F4}";
+                case "f5":
+                    return "{F5}";
+                case "f6":
+                    return "{F6}";
+                case "f7":
+                    return "{F7}";
+                case "f8":
+                    return "{F8}";
+                case "f9":
+                    return "{F9}";
+                case "f10":
+                    return "{F10}";
+                case "f11":
+                    return "{F11}";
+                case "f12":
+                    return "{F12}";
+                case "f13":
+                    return "{F13}";
+                case "f14":
+                    return "{F14}";
+                case "f15":
+                    return "{F15}";
+                case "f16":
+                    return "{F16}";
+                case "multiply":
+                    return "{MULTIPLY}";
+                case "add":
+                    return "{ADD}";
+                case "subtract":
+                    return "{SUBTRACT}";
+                case "divide":
+                    return "{DIVIDE}";
+                default:
+                    return key.ToLower();
+            }
+        }
 
         // Activate an application window.
         [DllImport("USER32.DLL")]
